415f59fbffb8421c29ab88bf73c677d7430130dd
ess-maker <essh4014@gmail.com>
2024-01-24 08:31:44 +0100

    'push-site'

--- assets/assets/js/script.js
+++
@@ -1,5 +0,0 @@
-const save__button  = document.getElementById('save__button')
-
-save__button.addEventListener('click' , () => {
-    return console.log('hamza is the best');
-} )
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
-const save__button  = document.getElementById('save__button')
+const side_menu = document.getElementById('side-menu');
+const side = document.getElementById('sidebar-menu');
+const shadow = document.querySelector('.bg-shadow');
 
-save__button.addEventListener('click' , () => {
-    return console.log('hamza is the best');
-} )
+side_menu.addEventListener('click' , () => {
+    side.classList.add('active');
+    shadow.toggleAttribute('hidden');
+})
+shadow.addEventListener('click' , () => {
+    side.classList.remove('active');
+    shadow.toggleAttribute('hidden');
+})
